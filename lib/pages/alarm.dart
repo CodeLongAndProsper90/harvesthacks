ca784eecc78228c0c0294ee198fce9bc51104e9d
Eli Nicksic <elinicksic@gmail.com>
2022-11-12 16:19:52 -0600

    Work on alarm page

--- lib/pages/alarm.dart
+++ lib/pages/alarm.dart
@@ -1,16 +1,44 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:just_audio/just_audio.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:risin/pages/inital.dart';
+import 'package:risin/pages/qrscanner.dart';
 import 'package:risin/system/compute_alarm.dart';
 import 'package:material_clock/material_clock.dart' as materialClock;
 
+enum AlarmStopMethod {
+  button,
+  qrscan
+}
+
 class AlarmPage extends StatefulWidget {
-  const AlarmPage({Key? key}) : super(key: key);
+  final AlarmStopMethod stopMethod;
+  const AlarmPage({Key? key, required this.stopMethod}) : super(key: key);
 
   @override
   State<AlarmPage> createState() => _AlarmPageState();
 }
 
 class _AlarmPageState extends State<AlarmPage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+  bool stopUnlocked = false;
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -31,6 +59,9 @@ class _AlarmPageState extends State<AlarmPage> {
             size: 200.0,
             time: DateTime.now(),
           ),
+          QRScanner(onScanned: () => {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => InitialPage()),)
+          }),
           const Expanded(child: SizedBox.shrink()),
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -40,7 +71,8 @@ class _AlarmPageState extends State<AlarmPage> {
                   onPressed: null,
                   child: Text(
                     "Stop",
-                    style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 190, 18, 18)),
+                    style: TextStyle(
+                        fontSize: 20, color: Color.fromARGB(255, 190, 18, 18)),
                   )),
               SizedBox(width: 40),
               TextButton(

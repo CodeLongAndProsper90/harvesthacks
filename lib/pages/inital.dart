5bb93239ff6f347d85d79f92e449e65ece69a216
Ellie Little <jupiter@m.rdis.dev>
2022-11-12 13:36:21 -0600

    fix up start page

--- lib/pages/inital.dart
+++ lib/pages/inital.dart
@@ -18,10 +18,10 @@ class _InitialPageState extends State<InitialPage> with TickerProviderStateMixin
 
   late final Animation<Offset> _offsetAnimation = Tween<Offset>(
     begin: Offset.zero,
-    end: const Offset(1.5, 0.0),
+    end: const Offset(0, -0.5),
   ).animate(CurvedAnimation(
     parent: _controller,
-    curve: Curves.elasticIn,
+    curve: Curves.bounceIn,
   ));
   @override
   void dispose() {
@@ -30,6 +30,35 @@ class _InitialPageState extends State<InitialPage> with TickerProviderStateMixin
   }
 
   Widget build(BuildContext context) {
+		var morning_grad = [
+			Colors.cyan.shade100,
+			Colors.yellow.shade100
+		];
+
+		var evening_grad = [
+			Colors.deepOrange.shade400,
+			Colors.yellow.shade100
+		];
+
+		var night_grad = [
+			Colors.grey.shade600,
+			Colors.indigo.shade900,
+			Colors.blue.shade900
+		];
+
+		Color textcolor = Colors.black;
+		List<Color> chosen;
+		DateTime n = DateTime.now();
+		if (n.hour >= 17)
+			chosen = evening_grad;
+		else if (n.hour >= 19) {
+				chosen = night_grad;
+				textcolor = Colors.white;
+			}
+		else
+			chosen = morning_grad;
+
+ 
     return Scaffold(
 			body: LayoutBuilder(
 			builder: (BuildContext context, BoxConstraints constraints) {
@@ -43,29 +72,28 @@ class _InitialPageState extends State<InitialPage> with TickerProviderStateMixin
 					gradient: LinearGradient(
 						begin: Alignment.bottomLeft,
 						end: Alignment.topRight,
-						colors: [
-							Colors.cyan.shade100,
-							Colors.yellow.shade100
-						]
+						colors: chosen
 					)
 				),
 				child: Center(
 					child: Column(
+						mainAxisAlignment: MainAxisAlignment.center,
+						crossAxisAlignment: CrossAxisAlignment.center,
 						children: [
+							Spacer(),
+							SlideTransition(
+								position: _offsetAnimation,
+								child: Image.asset("images/sun.png", width: 75, height: 75),
+								),
 							Text(
 								"Risin'",
 								style: TextStyle(
-									fontSize: 36.0
+									fontSize: 36.0,
+									color: textcolor
 								),
 							),
 							Text("Press me to start"),
-							SlideTransition(
-								position: _offsetAnimation,
-								child: const Padding(
-									padding: EdgeInsets.all(8.0),
-									child: FlutterLogo(size: 150.0),
-								),
-							)
+							Spacer(),
 						]
 						)
 					)

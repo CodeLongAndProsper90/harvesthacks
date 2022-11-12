63032862352930ef46663280173babbd3a98fd51
Eli Nicksic <elinicksic@gmail.com>
2022-11-12 10:40:28 -0600

    Fix typo

--- lib/main.dart
+++ lib/main.dart
@@ -87,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: InitalPage(),
+      body: InitialPage(),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
           print(compute([
